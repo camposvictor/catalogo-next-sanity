3debcb6460295a8d950ea104eb36cddfacd211e5
Victor Hugo <vhsouza606@gmail.com>
2023-07-14 21:54:55 -0300

    feat: category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,5 +1,9 @@
 import { getCategory } from '@/actions/get-category'
+import { getProductsByCategory } from '@/actions/get-products-by-category'
 import { Billboard } from '@/components/billboard'
+import { ProductGrid } from '@/components/product-grid'
+import category from '@/sanity/schemas/category-schema'
+import { Product } from '@/types/product'
 
 type CategoryPageProps = {
   params: {
@@ -8,11 +12,19 @@ type CategoryPageProps = {
 }
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
-  const category = await getCategory(params.categoryId)
+  const [products, category] = await Promise.all([
+    getProductsByCategory(params.categoryId),
+    getCategory(params.categoryId),
+  ])
 
   return (
     <div>
       <Billboard imageSrc={category.image} text={category.label} />
+      <ProductGrid
+        className="md:my12 my-6"
+        title={category.name}
+        items={products}
+      />
     </div>
   )
 }
