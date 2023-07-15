41dd6efeb175ec3d42b604168fd669daafb8997e
Victor Hugo <vhsouza606@gmail.com>
2023-07-14 21:53:41 -0300

    feat: product type

---
+++ types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  uniqueSize: boolean
+  name: string
+  id: string
+  images: string[]
+  colorCount?: number
+  sizeCount?: number
+  colors?: string[]
+  sizes?: string[]
+  price: number
+}
