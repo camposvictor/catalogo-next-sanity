2c2e46112c356c8e9fdbd24f8d33c41f106a0ac2
Victor Hugo <vhsouza606@gmail.com>
2023-07-14 21:55:08 -0300

    styles: billboard archive

--- actions/get-billboard.ts
+++ actions/get-billboard.ts
@@ -1,14 +1,16 @@
-import {client } from '@/sanity/lib/client'
+import { client } from '@/sanity/lib/client'
 import { groq } from 'next-sanity'
 
-export async function getBillboard(id : string) {
-    const billboard = await client.fetch(
-        groq`*[_id == $id]{
+export async function getBillboard(id: string) {
+  const billboard = await client.fetch(
+    groq`*[_id == $id]{
             _id,
             _createdAt,
             label,
             "image": image.asset->url,
-          }`, {id})
+          }`,
+    { id },
+  )
 
-    return billboard[0]
+  return billboard[0]
 }
